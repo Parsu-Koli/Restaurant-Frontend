40bd5d09723764241ba06d7a789493130b0975ab
Prashant Koli <koliprashant11112@gmail.com>
2026-07-15 12:15:28 +0530

    Somting Added

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,13 @@
+//Local Server
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:44398/api'
+// }
+
+//Production Server
+
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44398/api'
+  apiUrl: 'https://restaurant-api-bk12.onrender.com/api'
 }
